76cd25bc69e4b50b0de2ee489b304ca5fa2d5fcb
Yash <ymalegao@ucsc.edu>
2024-03-01 20:20:37 -0800

    commiting first WFC

---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Tile[] upNeighbour;
+    public Tile[] downNeighbour;
+    public Tile[] leftNeighbour;
+    public Tile[] rightNeighbour;
+    
+}
